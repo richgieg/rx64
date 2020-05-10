521f0ec5799fb21ec4d07e0faed4e11115814168
Richard Gieg <richgieg@users.noreply.github.com>
2020-05-10 09:33:23 -0700

    Use LibMemoryMap function instead of my own

--- src/main.c
+++ src/main.c
@@ -7,18 +7,15 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     
     // TODO: Load kernel image.
     
-    EFI_STATUS status;
     EFI_MEMORY_DESCRIPTOR *memoryMap;
-    UINTN mapSize;
-    UINTN descriptorSize;
+    UINTN noEntries;
     UINTN mapKey;
-    status = GetMemoryMap(&memoryMap, &mapSize, &descriptorSize, &mapKey);
-    if (status != EFI_SUCCESS) {
-        return status;
-    }
+    UINTN descriptorSize;
+    UINT32 descriptorVersion;
+    memoryMap = LibMemoryMap(&noEntries, &mapKey, &descriptorSize, &descriptorVersion);
 
     // Print memory map for my own curiosity.
-    PrintMemoryMap(memoryMap, mapSize, descriptorSize, TRUE);
+    PrintMemoryMap(memoryMap, noEntries, descriptorSize, TRUE);
 
     // TODO: Exit boot services and transfer control to kernel.
 
@@ -35,37 +32,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     for (;;) asm("hlt");
 }
 
-EFI_STATUS GetMemoryMap(EFI_MEMORY_DESCRIPTOR **MemoryMap, UINTN *MapSize, UINTN *DescriptorSize, UINTN *MapKey) {
-    EFI_STATUS status;
-    EFI_MEMORY_DESCRIPTOR *memoryMap = NULL;
-    UINTN mapSize = 0;
-    UINTN descriptorSize;
-    UINTN mapKey;
-    UINT32 descriptorVersion;
-    status = BS->GetMemoryMap(&mapSize, NULL, NULL, NULL, NULL);
-    while (status == EFI_BUFFER_TOO_SMALL) {
-        if (memoryMap) {
-            status = BS->FreePool(memoryMap);
-            if (status != EFI_SUCCESS) {
-                break;
-            }
-        }
-        status = BS->AllocatePool(EfiLoaderData, mapSize, (void **)&memoryMap);
-        if (status != EFI_SUCCESS) {
-            break;
-        }
-        status = BS->GetMemoryMap(&mapSize, memoryMap, &mapKey, &descriptorSize, &descriptorVersion);
-    }
-    if (status == EFI_SUCCESS) {
-        *MemoryMap = memoryMap;
-        *MapSize = mapSize;
-        *DescriptorSize = descriptorSize;
-        *MapKey = mapKey;
-    }
-    return status;
-}
-
-VOID PrintMemoryMap(EFI_MEMORY_DESCRIPTOR *MemoryMap, UINTN MapSize, UINTN DescriptorSize, BOOLEAN PrintDescriptors) {
+VOID PrintMemoryMap(EFI_MEMORY_DESCRIPTOR *MemoryMap, UINTN NoEntries, UINTN DescriptorSize, BOOLEAN PrintDescriptors) {
     const CHAR16 *memoryTypes[] = {
         L"EfiReservedMemoryType",
         L"EfiLoaderCode",
@@ -83,11 +50,12 @@ VOID PrintMemoryMap(EFI_MEMORY_DESCRIPTOR *MemoryMap, UINTN MapSize, UINTN Descr
         L"EfiPalCode",
         L"EfiMaxMemoryType"
     };
-    Print(L"Memory Map Size: %d\n", MapSize);
+    UINTN mapSize = NoEntries * DescriptorSize;
+    Print(L"Memory Map Size: %d\n", mapSize);
     Print(L"Descriptor Size: %d\n", DescriptorSize);
     if (PrintDescriptors) {
         UINT8 *startOfMemoryMap = (UINT8 *)MemoryMap;
-        UINT8 *endOfMemoryMap = startOfMemoryMap + MapSize;
+        UINT8 *endOfMemoryMap = startOfMemoryMap + mapSize;
         UINT8 *offset = startOfMemoryMap;
         UINT32 counter = 0;
         while (offset < endOfMemoryMap)
--- src/main.h
+++ src/main.h
@@ -1,18 +1,10 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS
-GetMemoryMap (
-    OUT EFI_MEMORY_DESCRIPTOR   **MemoryMap,
-    OUT UINTN                   *MapSize,
-    OUT UINTN                   *DescriptorSize,
-    OUT UINTN                   *MapKey
-    );
-
 VOID
 PrintMemoryMap (
     IN EFI_MEMORY_DESCRIPTOR   *MemoryMap,
-    IN UINTN                   MapSize,
+    IN UINTN                   NoEntries,
     IN UINTN                   DescriptorSize,
     IN BOOLEAN                 PrintDescriptors
     );
