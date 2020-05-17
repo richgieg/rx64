1760f65a41502998f5d7d1897ef1194ed83fdb06
Richard Gieg <richgieg@users.noreply.github.com>
2020-05-17 09:20:15 -0700

    Output control registers

--- src/loader/loader.c
+++ src/loader/loader.c
@@ -18,14 +18,38 @@ efi_main (
     // Initialize EFI library (Set BS, RT, and ST globals).
     // BS = Boot Services, RT = Runtime Services, ST = System Table.
     InitializeLib(ImageHandle, SystemTable);
-    // PrintEnvironmentVariables(FALSE);
-    // PrintMemoryMap(FALSE);
-    KernelEntry = LoadKernelImage(ImageHandle, L"kernel.elf");
-    // WaitForKeyStroke(L"\nPress any key to launch kernel...\n");
-    LoaderInfo = AllocatePool(sizeof(LOADER_INFO));
-    SetGraphicsMode(&LoaderInfo->Graphics);
-    ExitBootServices(ImageHandle);
-    KernelEntry(LoaderInfo);
+    // // PrintEnvironmentVariables(FALSE);
+    // // PrintMemoryMap(FALSE);
+    // KernelEntry = LoadKernelImage(ImageHandle, L"kernel.elf");
+    // // WaitForKeyStroke(L"\nPress any key to launch kernel...\n");
+    // LoaderInfo = AllocatePool(sizeof(LOADER_INFO));
+    // SetGraphicsMode(&LoaderInfo->Graphics);
+    // ExitBootServices(ImageHandle);
+    // KernelEntry(LoaderInfo);
+
+
+    UINT64 CR0 = 0;
+    UINT64 CR3 = 0;
+    UINT64 CR4 = 0;
+    asm(
+        "mov %%cr0, %%rax;"
+        "mov %%rax, %0;"
+        "mov %%cr3, %%rax;"
+        "mov %%rax, %1;"
+        "mov %%cr4, %%rax;"
+        "mov %%rax, %2;"
+
+        :"=r"(CR0),
+         "=r"(CR3),
+         "=r"(CR4)
+        :
+        :"%rax"
+    );
+
+    Print(L"CR0: %x\n", CR0);
+    Print(L"CR3: %x\n", CR3);
+    Print(L"CR4: %x\n", CR4);
+
 
     // Kernel should never return, but if it does...
     for (;;) {
