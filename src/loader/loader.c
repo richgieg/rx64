f95f778881ca0ed4173737997775c765d17f0234
Richard Gieg <richgieg@users.noreply.github.com>
2020-05-18 20:07:00 -0700

    Add demo mapping frame buffer virtual address

--- src/loader/loader.c
+++ src/loader/loader.c
@@ -6,6 +6,11 @@
 #include "util.h"
 #include "../kernel/loader_info.h"
 
+VOID
+MapFrameBuffer (
+    EFI_PHYSICAL_ADDRESS FrameBufferBase
+    );
+
 EFI_STATUS
 efi_main (
     IN EFI_HANDLE           ImageHandle,
@@ -25,17 +30,54 @@ efi_main (
     // // PrintMemoryMap(FALSE);
     // KernelEntry = LoadKernelImage(ImageHandle, L"kernel.elf");
     // // WaitForKeyStroke(L"\nPress any key to launch kernel...\n");
-    // LoaderInfo = AllocatePool(sizeof(LOADER_INFO));
-    // SetGraphicsMode(&LoaderInfo->Graphics);
+    LoaderInfo = AllocatePool(sizeof(LOADER_INFO));
+    SetGraphicsMode(&LoaderInfo->Graphics);
     // ExitBootServices(ImageHandle);
     // KernelEntry(LoaderInfo);
 
-    PrintMemoryMap(TRUE);
-    PrintControlRegisters();
-    PrintPml4Table(FALSE, FALSE);
+    // PrintMemoryMap(TRUE);
+    // PrintControlRegisters();
+    // PrintPml4Table(FALSE, FALSE);
+
+    MapFrameBuffer(LoaderInfo->Graphics.FrameBufferBase);
 
     // Kernel should never return, but if it does...
     for (;;) {
         asm("hlt");
+    }
+}
+
+VOID
+MapFrameBuffer (
+    EFI_PHYSICAL_ADDRESS FrameBufferBase
+    )
+{
+    UINT64 *Pml4Table;
+    UINT64 *PdpTable;
+    UINT64 *PageDirectory;
+    UINT64 *PageTable;
+    EFI_PHYSICAL_ADDRESS NewPages;
+    UINT32 *VirtualFrameBuffer;
+    int i;
+
+    BS->AllocatePages(AllocateAnyPages, EfiLoaderData, 3, &NewPages);
+    Pml4Table = (UINT64 *)GetPml4TableAddress();
+    PdpTable = (UINT64 *)NewPages;
+    PageDirectory = (UINT64 *)(NewPages + 4096);
+    PageTable = (UINT64 *)(NewPages + 8192);
+
+    PageTable[0] = FrameBufferBase & 0x0000fffffffff000 | 0x23;
+    Print(L"%x\n", PageTable[0]);
+    PageDirectory[0] = (UINT64)PageTable & 0x0000fffffffff000 | 0x23;
+    Print(L"%x\n", PageDirectory[0]);
+    PdpTable[0] = (UINT64)PageDirectory & 0x0000fffffffff000 | 0x23;
+    Print(L"%x\n", PdpTable[0]);
+    Pml4Table[256] = (UINT64)PdpTable & 0x0000fffffffff000 | 0x23;
+    Print(L"%x\n", Pml4Table[256]);
+
+    VirtualFrameBuffer = (UINT32 *)0xffff800000000000;
+    for (i = 0; i < 1024; i++) {
+        *VirtualFrameBuffer = 0xff0000;
+        VirtualFrameBuffer++;
     }
 }
