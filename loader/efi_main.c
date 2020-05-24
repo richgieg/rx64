0fbcec300ffd0434ceabc973e52aabe37a9c56c8
Richard Gieg <richgieg@users.noreply.github.com>
2020-05-24 11:07:27 -0700

    Use IMAGE_DOS_HEADER from winnt.h

--- loader/efi_main.c
+++ loader/efi_main.c
@@ -20,36 +20,36 @@ typedef struct _IMAGE_DATA_DIRECTORY {
 } IMAGE_DATA_DIRECTORY;
 
 typedef struct _IMAGE_OPTIONAL_HEADER64 {
-    UINT16        Magic;
-    UINT8        MajorLinkerVersion;
-    UINT8        MinorLinkerVersion;
-    UINT32       SizeOfCode;
-    UINT32       SizeOfInitializedData;
-    UINT32       SizeOfUninitializedData;
-    UINT32       AddressOfEntryPoint;
-    UINT32       BaseOfCode;
-    UINT64   ImageBase;
-    UINT32       SectionAlignment;
-    UINT32       FileAlignment;
-    UINT16        MajorOperatingSystemVersion;
-    UINT16        MinorOperatingSystemVersion;
-    UINT16        MajorImageVersion;
-    UINT16        MinorImageVersion;
-    UINT16        MajorSubsystemVersion;
-    UINT16        MinorSubsystemVersion;
-    UINT32       Win32VersionValue;
-    UINT32       SizeOfImage;
-    UINT32       SizeOfHeaders;
-    UINT32       CheckSum;
-    UINT16        Subsystem;
-    UINT16        DllCharacteristics;
-    UINT64   SizeOfStackReserve;
-    UINT64   SizeOfStackCommit;
-    UINT64   SizeOfHeapReserve;
-    UINT64   SizeOfHeapCommit;
-    UINT32       LoaderFlags;
-    UINT32       NumberOfRvaAndSizes;
-    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+    UINT16                  Magic;
+    UINT8                   MajorLinkerVersion;
+    UINT8                   MinorLinkerVersion;
+    UINT32                  SizeOfCode;
+    UINT32                  SizeOfInitializedData;
+    UINT32                  SizeOfUninitializedData;
+    UINT32                  AddressOfEntryPoint;
+    UINT32                  BaseOfCode;
+    UINT64                  ImageBase;
+    UINT32                  SectionAlignment;
+    UINT32                  FileAlignment;
+    UINT16                  MajorOperatingSystemVersion;
+    UINT16                  MinorOperatingSystemVersion;
+    UINT16                  MajorImageVersion;
+    UINT16                  MinorImageVersion;
+    UINT16                  MajorSubsystemVersion;
+    UINT16                  MinorSubsystemVersion;
+    UINT32                  Win32VersionValue;
+    UINT32                  SizeOfImage;
+    UINT32                  SizeOfHeaders;
+    UINT32                  CheckSum;
+    UINT16                  Subsystem;
+    UINT16                  DllCharacteristics;
+    UINT64                  SizeOfStackReserve;
+    UINT64                  SizeOfStackCommit;
+    UINT64                  SizeOfHeapReserve;
+    UINT64                  SizeOfHeapCommit;
+    UINT32                  LoaderFlags;
+    UINT32                  NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY    DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
 } IMAGE_OPTIONAL_HEADER64;
 
 typedef struct _IMAGE_NT_HEADERS64 {
@@ -58,10 +58,27 @@ typedef struct _IMAGE_NT_HEADERS64 {
     IMAGE_OPTIONAL_HEADER64     OptionalHeader;
 } IMAGE_NT_HEADERS64;
 
-typedef struct _MSDOS_STUB {
-    UINT8   Ignored[0x3c];
-    UINT32  PeOffset;
-} MSDOS_STUB;
+typedef struct _IMAGE_DOS_HEADER {      // DOS .EXE header
+    UINT16   e_magic;                     // Magic number
+    UINT16   e_cblp;                      // Bytes on last page of file
+    UINT16   e_cp;                        // Pages in file
+    UINT16   e_crlc;                      // Relocations
+    UINT16   e_cparhdr;                   // Size of header in paragraphs
+    UINT16   e_minalloc;                  // Minimum extra paragraphs needed
+    UINT16   e_maxalloc;                  // Maximum extra paragraphs needed
+    UINT16   e_ss;                        // Initial (relative) SS value
+    UINT16   e_sp;                        // Initial SP value
+    UINT16   e_csum;                      // Checksum
+    UINT16   e_ip;                        // Initial IP value
+    UINT16   e_cs;                        // Initial (relative) CS value
+    UINT16   e_lfarlc;                    // File address of relocation table
+    UINT16   e_ovno;                      // Overlay number
+    UINT16   e_res[4];                    // Reserved words
+    UINT16   e_oemid;                     // OEM identifier (for e_oeminfo)
+    UINT16   e_oeminfo;                   // OEM information; e_oemid specific
+    UINT16   e_res2[10];                  // Reserved words
+    UINT32   e_lfanew;                    // File address of new exe header
+} IMAGE_DOS_HEADER;
 
 EFI_STATUS
 LoadKernelImage (
@@ -170,9 +187,9 @@ LoadKernelImage (
         return Status;
     }
 
-    MSDOS_STUB *MsdosStub = KernelBuffer;
+    IMAGE_DOS_HEADER *DosHeader = KernelBuffer;
     IMAGE_NT_HEADERS64 *PeHeader =
-        (IMAGE_NT_HEADERS64 *)((UINT64)MsdosStub + MsdosStub->PeOffset);
+        (IMAGE_NT_HEADERS64 *)((UINT64)DosHeader + DosHeader->e_lfanew);
 
     Print(L"Sections: %d\n", PeHeader->FileHeader.NumberOfSections);
     Print(L"Size of Optional Header: %d\n\n", PeHeader->FileHeader.SizeOfOptionalHeader);
