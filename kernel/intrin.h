b20e491943e64c46f7125677c874add6eb108877
Richard Gieg <richgieg@users.noreply.github.com>
2020-05-25 18:54:07 -0700

    Add clean intrin.h to kernel

---
+++ kernel/intrin.h
@@ -0,0 +1,13 @@
+// intrin.h
+//
+// Intrinsic functions in Microsoft C++ compiler that are used by the kernel.
+//
+// List of all intrinsic functions:
+// https://docs.microsoft.com/en-us/cpp/intrinsics/x64-amd64-intrinsics-list
+//
+
+#pragma once
+
+#include <types.h>
+
+VOID    __halt();
